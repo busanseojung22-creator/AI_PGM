eacf6fa96e1104ab08d92fd623695c20fcd0b35b
busanseojung22-creator <busanseojung22@gmail.com>
2026-06-26 16:46:04 +0900

    Add files via upload

---
+++ 0626/select03-1.py
@@ -0,0 +1,7 @@
+year = int(input("연도 입력 : "))
+
+if year %4 == 0 and year %100 != 0 or year % 400 == 0:
+    print(f"{year}년은 윤년(leap year)입니다.")
+else:
+    print(f"{year}년은 평년(common year)입니다.")
+        
