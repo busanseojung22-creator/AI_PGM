044aa4f2a9f526a2f57f931c939f10896c6795d4
busanseojung22-creator <busanseojung22@gmail.com>
2026-06-26 12:37:10 +0900

    Add files via upload
    0626

---
+++ 0626/stringEX.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first numbe: "))
+b = int(input("Enter Second numbe: "))
+c = int(input("Enter third numbe: "))
+d = int(input("Enter forth numbe: "))
+e = int(input("Enter fifth numbe: "))
+
+print(f"You entered: {a} and {b}")
+print(f"a + b = {(a) + (b)}")
+print(f"a - b = {(a) - (b)}")
+print(f"a * b = {(a) * (b)}")
+print(f"a / b = {(a) / (b)}")
+
+print(f"all+ = {(a) + (b) + (c) + (d) + (e)}")
+print(f"av = {{(a) + (b) + (c) + (d) + (e)}/5}")
